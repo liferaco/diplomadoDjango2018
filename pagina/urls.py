1229be6e76e9f8f1fe46dbdab963fcb991a25469
Lizeth Fernanda Ramirez Cortes <liferaco9625@gmail.com>
2018-06-18 22:12:18 -0500

    video 11. generacion de un reporte pdf

--- pagina/urls.py
+++ pagina/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from pagina.views import index,producto_view,producto_list,producto_update,producto_delete,productoList,productoCreate,productoUpdate,productoDelete
+from pagina.views import index,producto_view,producto_list,producto_update,producto_delete,productoList,productoCreate,productoUpdate,productoDelete,ReportePersonasPDF
+from django.contrib.auth.decorators import login_required
 
 app_name = 'pagina'
 
@@ -9,5 +10,5 @@ urlpatterns = [
     path('listar',productoList.as_view(),name='producto_listar'),
     path('editar/<pk>',productoUpdate.as_view(),name='producto_editar'),
     path('eliminar/<pk>',productoDelete.as_view(),name='producto_eliminar'),
-
+    path('reporte_personas_pdf',ReportePersonasPDF.as_view(), name="reporte_personas_pdf"),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -5,6 +5,13 @@ from django.urls import reverse_lazy
 from pagina.forms import productoForm
 from pagina.models import producto
 
+from django.conf import settings
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from django.views.generic import View
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib.units import cm
+from reportlab.lib import colors
 
 # Create your views here.
 def index(request):
@@ -67,3 +74,82 @@ class productoDelete(DeleteView):
 	model = producto
 	template_name = 'pagina/productoDelete.html'
 	success_url = reverse_lazy('pagina:producto_listar')
+
+class ReportePersonasPDF(View):
+	
+	def cabecera(self,pdf):
+		#Utilizamos el archivo logo_django.png que está guardado en la carpeta media/imagenes
+		archivo_imagen = settings.MEDIA_ROOT+'/imagenes/carro.png'
+		#Definimos el tamaño de la imagen a cargar y las coordenadas correspondientes
+		pdf.drawImage(archivo_imagen, 40, 750, 120, 90,preserveAspectRatio=True)
+		#Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
+		pdf.setFont("Helvetica", 16)
+		#Dibujamos una cadena en la ubicación X,Y especificada
+		pdf.drawString(230, 790, u"CASA DEL REPUESTO")
+		pdf.setFont("Helvetica", 14)
+		pdf.drawString(224, 770, u"REPORTE DE PRODUCTOS")
+
+	def get(self, request, *args, **kwargs):
+		#Indicamos el tipo de contenido a devolver, en este caso un pdf
+		
+		#La clase io.BytesIO permite tratar un array de bytes como un fichero binario, se utiliza como almacenamiento temporal
+		buffer = BytesIO()
+		#Canvas nos permite hacer el reporte con coordenadas X y Y
+		pdf = canvas.Canvas(buffer)
+		#Llamo al método cabecera donde están definidos los datos que aparecen en la cabecera del reporte.
+		self.cabecera(pdf)
+		#Con show page hacemos un corte de página para pasar a la siguiente
+		# pdf.showPage()
+		nombre_archivo ="ReporteProductos.pdf"
+		#Definimos que el tipo de respuesta a devolver es un archivo de microsoft excel
+		response = HttpResponse(content_type="application/pdf")
+		contenido = "attachment; filename={0}".format(nombre_archivo)
+		response["Content-Disposition"] = contenido
+		pdf.save(response)
+		pdf = buffer.getvalue()
+		buffer.close()
+		response.write(pdf)
+		return response
+
+	def tabla(self,pdf,y):
+		#Creamos una tupla de encabezados para neustra tabla
+		encabezados = ('N', 'Codigo', 'Nombre', 'Tipo','Costo','Marca')
+		#Creamos una lista de tuplas que van a contener a las personas
+		detalles = [(producto.id, producto.codigo, producto.nombre, producto.tipo,producto.costo,producto.idMarca) for producto in producto.objects.all()]
+		#Establecemos el tamaño de cada una de las columnas de la tabla
+		detalle_orden = Table([encabezados] + detalles, colWidths=[1 * cm, 3 * cm, 4 * cm, 2 * cm, 4 * cm, 3 * cm])
+		#Aplicamos estilos a las celdas de la tabla
+		detalle_orden.setStyle(TableStyle(
+		[
+			#La primera fila(encabezados) va a estar centrada
+			('ALIGN',(0,0),(3,0),'CENTER'),
+			#Los bordes de todas las celdas serán de color negro y con un grosor de 1
+			('GRID', (0, 0), (-1, -1), 1, colors.black),
+			#El tamaño de las letras de cada una de las celdas será de 10
+			('FONTSIZE', (0, 0), (-1, -1), 10),
+			]
+		))
+		#Establecemos el tamaño de la hoja que ocupará la tabla
+		detalle_orden.wrapOn(pdf, 800, 600)
+		#Definimos la coordenada donde se dibujará la tabla
+		detalle_orden.drawOn(pdf, 60,y)
+
+	def get(self, request, *args, **kwargs):
+		#Indicamos el tipo de contenido a devolver, en este caso un pdf
+		response = HttpResponse(content_type='application/pdf')
+		#La clase io.BytesIO permite tratar un array de bytes como un fichero binario, se utiliza como almacenamiento temporal
+		buffer = BytesIO()
+		#Canvas nos permite hacer el reporte con coordenadas X y Y
+		pdf = canvas.Canvas(buffer)
+		#Llamo al método cabecera donde están definidos los datos que aparecen en la cabecera del reporte.
+		self.cabecera(pdf)
+		y = 600
+		self.tabla(pdf, y)
+		#Con show page hacemos un corte de página para pasar a la siguiente
+		pdf.showPage()
+		pdf.save()
+		pdf = buffer.getvalue()
+		buffer.close()
+		response.write(pdf)
+		return response
+
